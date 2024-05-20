19b95c445e72f475272365c9495b6668b0c8507d
Devsstudent <devsstudent777@gmail.com>
2024-05-20 18:46:14 +0200

    cleaned the code, ready to push

--- getPriceFromMileage.go
+++ getPriceFromMileage.go
@@ -10,31 +10,29 @@ import (
 )
 
 func main() {
+	var theta0, theta1 float64 = 0.0, 0.0;
 	file, err := os.Open("./tetaInfo");
-	if (err != nil) {
-		log.Fatal(err);
+	if (err == nil) {
+		scanner := bufio.NewScanner(file);
+		var i = 0;
+		for scanner.Scan() {
+			if (i == 1) {
+				log.Fatal("too much line in tetaInfo");
+			}
+			var line = strings.Split(scanner.Text(), ",");
+			if (len(line) < 2) {
+				log.Fatal("Wrong tetaInfo");
+			}
+			theta0, err = strconv.ParseFloat(line[0], 10);
+			theta1, err = strconv.ParseFloat(line[1], 10);
+			if (err != nil) {
+				log.Fatal("Error theta Format");
+			}
+			i++;
+		}
 	}
 	defer file.Close();
-	var theta0, theta1 float64;
-
-	scanner := bufio.NewScanner(file);
-	var i = 0;
-	for scanner.Scan() {
-		if (i == 1) {
-			log.Fatal("too much line in tetaInfo");
-		}
-		var line = strings.Split(scanner.Text(), ",");
-		if (len(line) < 2) {
-			log.Fatal("Wrong tetaInfo");
-		}
-		theta0, err = strconv.ParseFloat(line[0], 10);
-		theta1, err = strconv.ParseFloat(line[1], 10);
-		if (err != nil) {
-			log.Fatal("Error theta Format");
-		}
-		i++;
-	}
-	fmt.Printf("Enter a mileage :");
+	fmt.Printf("Enter a mileage: ");
 	var input string
 	_, err = fmt.Scanf("%s\n", &input)
 	if (err != nil) {
--- training.go
+++ training.go
@@ -3,27 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"sort"
 	"log"
 	"bufio"
 	"strconv"
 	"math"
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
-	//"github.com/go-echarts/go-echarts/v2/render"
 	"strings")
-
-//resume of the linear regression :
-//It is the form Y = a + bX
-//To get a : (somme de x) / n - b * (somme de y) / n
-//To get b : (some des combine xy (en moyenne)) - somme des combine / n
-//Diviser par la somme des x^2 - somme des x^2 / n
-
-//Il nous faut donc la somme des x^2
-
-//La somme de x, et de y
-
-//La somme de x par y
 
 type Coord struct {
 	x float64
@@ -31,74 +17,54 @@ type Coord struct {
 }
 
 func costFunction(predictions []float64, points []Coord) float64 {
-	var diff float64
+	var diff float64;
 	for i := range predictions {
-		d := predictions[i] - points[i].y
-		diff += math.Pow(d, 2.0)
+		d := predictions[i] - points[i].y;
+		diff += math.Pow(d, 2.0);
 	}
-	loss := diff / float64(len(points))
-	return loss
+	loss := diff / float64(len(points));
+	return loss;
 }
 
 func findMinMax(points []Coord) (minX, maxX, minY, maxY float64) {
-	minX, maxX = points[0].x, points[0].x
-	minY, maxY = points[0].y, points[0].y
+	minX, maxX = points[0].x, points[0].x;
+	minY, maxY = points[0].y, points[0].y;
 	for _, point := range points {
 		if point.x < minX {
-			minX = point.x
+			minX = point.x;
 		}
 		if point.x > maxX {
-			maxX = point.x
+			maxX = point.x;
 		}
 		if point.y < minY {
-			minY = point.y
+			minY = point.y;
 		}
 		if point.y > maxY {
-			maxY = point.y
+			maxY = point.y;
 		}
 	}
-	return
+	return;
 }
 
 func minMaxNormalization(points []Coord) []Coord {
-	// Find the minimum and maximum values for x and y
-	var minX, maxX, minY, maxY float64
-	minX, maxX = points[0].x, points[0].x
-	minY, maxY = points[0].y, points[0].y
+	minX, maxX, minY, maxY := findMinMax(points);
 
-	for _, point := range points {
-		if point.x < minX {
-			minX = point.x
-		}
-		if point.x > maxX {
-			maxX = point.x
-		}
-		if point.y < minY {
-			minY = point.y
-		}
-		if point.y > maxY {
-			maxY = point.y
-		}
-	}
-
-	// Apply Min-Max normalization to both x and y values
 	normalizedPoints := make([]Coord, len(points))
 	for i, point := range points {
-		normalizedX := (point.x - minX) / (maxX - minX)
-		normalizedY := (point.y - minY) / (maxY - minY)
-		normalizedPoints[i] = Coord{x: normalizedX, y: normalizedY}
+		normalizedX := (point.x - minX) / (maxX - minX);
+		normalizedY := (point.y - minY) / (maxY - minY);
+		normalizedPoints[i] = Coord{x: normalizedX, y: normalizedY};
 	}
-
-	return normalizedPoints
+	return normalizedPoints;
 }
 
 func denormalizeTheta(theta0, theta1 float64, originalPoints []Coord) (newTheta0, newTheta1 float64) {
-	minX, maxX, minY, maxY := findMinMax(originalPoints)
-	rangeX := maxX - minX
-	rangeY := maxY - minY
+	minX, maxX, minY, maxY := findMinMax(originalPoints);
+	rangeX := maxX - minX;
+	rangeY := maxY - minY;
 
-	newTheta1 = theta1 * rangeY / rangeX
-	newTheta0 = theta0 * rangeY + minY - newTheta1 * minX
+	newTheta1 = theta1 * rangeY / rangeX;
+	newTheta0 = theta0 * rangeY + minY - newTheta1 * minX;
 	return
 }
 
@@ -107,8 +73,7 @@ func calcGradientTeta1(points []Coord, teta0, teta1 float64) float64 {
 	for i := 0; i < len(points); i++ {
 		diff += ((teta0 + teta1 * points[i].y) - points[i].x) * points[i].y;
 	}
-//	fmt.Println("ICI", predicted, points, "END", diff);
-	return (diff / float64(len(points)))
+	return (diff / float64(len(points)));
 }
 
 func calcGradientTeta0(points []Coord, teta0, teta1 float64) float64 {
@@ -116,16 +81,16 @@ func calcGradientTeta0(points []Coord, teta0, teta1 float64) float64 {
 	for i := 0; i < len(points); i++ {
 		diff += (teta0 + teta1 * points[i].y) - points[i].x;
 	}
-	return (diff / float64(len(points)))
+	return (diff / float64(len(points)));
 }
 
 func f(points []Coord, teta0 float64, teta1 float64) []float64 {
 	var result []float64
 	for _, val := range points {
 		y := teta1 * val.x + teta0;
-		result = append(result, y)
+		result = append(result, y);
 	}
-	return result
+	return (result);
 }
 
 func main() {
@@ -138,7 +103,6 @@ func main() {
 	scanner := bufio.NewScanner(file);
 	var points []Coord;
 	var n = 0;
-	var kms sort.Float64Slice;
 	var km, price float64;
 	var pointValues []opts.ScatterData;
 	for scanner.Scan() {
@@ -154,51 +118,29 @@ func main() {
 		if (err != nil) {
 			log.Fatal(err);
 		}
-		points = append(points, Coord{x: km, y: price})
-;
-		pointValues = append(pointValues, opts.ScatterData{Value: []interface{}{km, price} , SymbolSize: 10, XAxisIndex: int(km), YAxisIndex: int(price)});
+		points = append(points, Coord{x: km, y: price});
 
-		kms = append(kms, km);
+		pointValues = append(pointValues, opts.ScatterData{Value: []interface{}{km, price} , SymbolSize: 10, XAxisIndex: int(km), YAxisIndex: int(price)});
 		n++;
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err);
 	}
-	file.Close();
-	
 
-	//Checker si thetaFIle est la
-	//Si oui on recupere les valeur de teta
-	//On defini un nombre d'iteration
 	var iteration = 100000;
 	var tetha0 float64= 0.0;
 	var tetha1 float64= 0.0;
 	var learningRate = 0.01;
 
-	//Ensuite on va loopp sur les data {iteration} fois
-
-	//Loss gradient = 
-
-	//De calculer la sommes des slopes et des intercept
-
-	//Grace au resultat on obtien 2 step size = Slope / Intercept * Learning Rate
-
-	//On a donc deux nouvelle pour tetha0 (intercept) et tetha1(slope)
-
-	//Et on recommence
-
-	//SOit jusqua la fin iteration max
-
-	//Soit quand next step est < 0.009
 	pointsNormed := minMaxNormalization(points);
 	for i := 0; i < iteration; i++ {
 		gradTeta0 := calcGradientTeta0(pointsNormed, tetha0, tetha1);
 		gradTeta1 := calcGradientTeta1(pointsNormed, tetha0, tetha1);
 		tetha0 -= gradTeta0 * learningRate;
 		tetha1 -= gradTeta1 * learningRate;
-		prediction := f(pointsNormed, tetha0, tetha1);
-		cost := costFunction(prediction, pointsNormed);
-		fmt.Println("COST :", cost);
+	//	prediction := f(pointsNormed, tetha0, tetha1);
+	//	cost := costFunction(prediction, pointsNormed);
+	//	fmt.Println("COST :", cost);
 	}
 
 	a, b := denormalizeTheta(tetha0, tetha1, points);
@@ -215,13 +157,10 @@ func main() {
         charts.WithYAxisOpts(opts.YAxis{Name: "Price"}),
     );
 	scatter := 	charts.NewScatter();
-	kms.Sort();
 	var lineInfo []opts.LineData;
 	for _, point := range points {
 		lineInfo = append(lineInfo, opts.LineData{Value: []interface{}{point.x, a + (b * point.x)}});
 	}
-	//Denormalized Y
-	//Append it
      line.AddSeries("MyLine", lineInfo,
             charts.WithLineChartOpts(
                 opts.LineChart{Smooth: true},
